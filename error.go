570614a2d895ac319762ab5a5f9f31456e5d5b49
Yuki Iwanaga <yuki@creasty.com>
2017-08-05 20:28:57 +0900

    Create base

---
+++ error.go
@@ -0,0 +1,105 @@
+package apperrors
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Error is an error that has an application context
+type Error struct {
+	Err        error
+	Message    string
+	StatusCode int
+	Report     bool
+	StackTrace StackTrace
+}
+
+// New returns an error with the supplied message
+func New(str string) error {
+	return &Error{
+		Err:        errors.New(str),
+		StackTrace: newStackTrace(0),
+	}
+}
+
+// Errorf formats according to a format specifier and returns the string
+// as a value that satisfies error
+func Errorf(format string, args ...interface{}) error {
+	return &Error{
+		Err:        fmt.Errorf(format, args...),
+		StackTrace: newStackTrace(0),
+	}
+}
+
+// Error implements error interface
+func (e *Error) Error() string {
+	if e.Message != "" {
+		return e.Message
+	}
+
+	return e.Err.Error()
+}
+
+// Copy creates a copy of the current object
+func (e *Error) Copy() *Error {
+	return &Error{
+		Err:        e.Err,
+		Message:    e.Message,
+		StatusCode: e.StatusCode,
+		Report:     e.Report,
+		StackTrace: e.StackTrace,
+	}
+}
+
+// WithMessage wraps err if necessary, and sets a message to its context
+func WithMessage(err error, msg string) error {
+	if err == nil {
+		return nil
+	}
+
+	appErr := wrap(err).Copy()
+	appErr.Message = msg
+	return appErr
+}
+
+// WithStatusCode wraps err if necessary, and sets a status code to its context
+func WithStatusCode(err error, code int) error {
+	if err == nil {
+		return nil
+	}
+
+	appErr := wrap(err).Copy()
+	appErr.StatusCode = code
+	return appErr
+}
+
+// WithReport wraps err if necessary, and marks as a reportable
+func WithReport(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	appErr := wrap(err).Copy()
+	appErr.Report = true
+	return appErr
+}
+
+func wrap(err error) *Error {
+	if appErr, ok := err.(*Error); ok {
+		return appErr
+	}
+
+	return &Error{
+		Err:        err,
+		StackTrace: newStackTrace(1),
+	}
+}
+
+// Unwrap extracts underlying apperrors.Error from an error
+func Unwrap(err error) *Error {
+	if appErr, ok := err.(*Error); ok {
+		return appErr
+	}
+
+	return nil
+}
---
+++ error_test.go
@@ -0,0 +1,173 @@
+package apperrors
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	err := New("message")
+	assert.Equal(t, "message", err.Error())
+
+	appErr := Unwrap(err)
+	assert.Equal(t, err.Error(), appErr.Err.Error())
+	assert.Equal(t, "", appErr.Message)
+}
+
+func TestErrorf(t *testing.T) {
+	err := Errorf("message %d", 123)
+	assert.Equal(t, "message 123", err.Error())
+
+	appErr := Unwrap(err)
+	assert.Equal(t, err.Error(), appErr.Err.Error())
+	assert.Equal(t, "", appErr.Message)
+}
+
+func TestWithMessage(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		err := WithMessage(nil, "message")
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("bare", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := WithMessage(err0, "message")
+		assert.Equal(t, "message", err1.Error())
+
+		appErr := Unwrap(err1)
+		assert.Equal(t, err0, appErr.Err)
+		assert.Equal(t, err1.Error(), appErr.Message)
+	})
+
+	t.Run("already wrapped", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := &Error{
+			Err:        err0,
+			Message:    "message 1",
+			StatusCode: 400,
+		}
+		err2 := WithMessage(err1, "message 2")
+		assert.Equal(t, "message 2", err2.Error())
+
+		{
+			appErr := Unwrap(err1)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, err1.Error(), appErr.Message)
+			assert.Equal(t, 400, appErr.StatusCode)
+		}
+
+		{
+			appErr := Unwrap(err2)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, err2.Error(), appErr.Message)
+			assert.Equal(t, 400, appErr.StatusCode)
+		}
+	})
+}
+
+func TestWithStatusCode(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		err := WithStatusCode(nil, 200)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("bare", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := WithStatusCode(err0, 200)
+
+		appErr := Unwrap(err1)
+		assert.Equal(t, err0, appErr.Err)
+		assert.Equal(t, "", appErr.Message)
+	})
+
+	t.Run("already wrapped", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := &Error{
+			Err:        err0,
+			Message:    "message 1",
+			StatusCode: 400,
+		}
+		err2 := WithStatusCode(err1, 500)
+
+		{
+			appErr := Unwrap(err1)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, err1.Error(), appErr.Message)
+			assert.Equal(t, 400, appErr.StatusCode)
+		}
+
+		{
+			appErr := Unwrap(err2)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, err1.Error(), appErr.Message)
+			assert.Equal(t, 500, appErr.StatusCode)
+		}
+	})
+}
+
+func TestWithReport(t *testing.T) {
+	t.Run("nil", func(t *testing.T) {
+		err := WithReport(nil)
+		assert.Equal(t, nil, err)
+	})
+
+	t.Run("bare", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := WithReport(err0)
+
+		appErr := Unwrap(err1)
+		assert.Equal(t, err0, appErr.Err)
+		assert.Equal(t, "", appErr.Message)
+	})
+
+	t.Run("already wrapped", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := WithReport(err0)
+		err2 := WithReport(err1)
+
+		{
+			appErr := Unwrap(err1)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, true, appErr.Report)
+		}
+
+		{
+			appErr := Unwrap(err2)
+			assert.Equal(t, err0, appErr.Err)
+			assert.Equal(t, true, appErr.Report)
+		}
+	})
+}
+
+func TestWrap(t *testing.T) {
+	t.Run("bare", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := wrap(err0)
+		assert.Equal(t, "original", err1.Error())
+
+		appErr := Unwrap(err1)
+		assert.Equal(t, err0, appErr.Err)
+		assert.Equal(t, "", appErr.Message)
+	})
+
+	t.Run("already wrapped", func(t *testing.T) {
+		err0 := errors.New("original")
+
+		err1 := wrap(err0)
+		err2 := wrap(err1)
+		assert.Equal(t, "original", err2.Error())
+
+		appErr := Unwrap(err2)
+		assert.Equal(t, err0, appErr.Err)
+		assert.Equal(t, "", appErr.Message)
+	})
+}
